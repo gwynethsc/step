92acfc03f138d7025d644d0be6e326afc1531e4f
Gwyneth Chen <gwyneth.stella.chen@gmail.com>
2020-06-18 21:39:25 +0000

    Extract out a helper addMeetingOption for readability

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -89,18 +89,16 @@ public final class FindMeetingQuery {
         List<AvailabilityChange> changes = getChangesInAvailabilityFromEvents(events);
 
         Set<String> currentAttendees = new HashSet<String>();
-        List<TimeRange> openings = new ArrayList<TimeRange>();
+        List<TimeRange> meetingOptions = new ArrayList<TimeRange>();
         int slotStart = 0;
         for (AvailabilityChange change : changes) {
 
             Collection<String> consideredAttendees = request.getAttendees();
             applyAvailabilityChange(currentAttendees, change, consideredAttendees);
             
-            // Use change information to open or close a new potential slot, if applicable
+            // Use change information to open or close a new potential slot, as applicable
             if (slotStart >= 0 && !currentAttendees.isEmpty()) {
-                TimeRange openSlot = TimeRange.fromStartEnd(slotStart, change.timestamp(), false);
-                if (openSlot.duration() >= request.getDuration())
-                    openings.add(openSlot);
+                addMeetingOption(meetingOptions, slotStart, change.timestamp(), request.getDuration());
                 slotStart = -1;
             } else if (slotStart < 0 && currentAttendees.isEmpty()) {
                 slotStart = change.timestamp();
@@ -109,12 +107,10 @@ public final class FindMeetingQuery {
 
         // Add end-of-day slot, if applicable
         if (slotStart >= 0) {
-            TimeRange openSlot = TimeRange.fromStartEnd(slotStart, TimeRange.END_OF_DAY, true);
-            if (openSlot.duration() >= request.getDuration())
-                openings.add(openSlot);
+            addMeetingOption(meetingOptions, slotStart, TimeRange.END_OF_DAY + 1, request.getDuration());
         }
 
-        return openings;
+        return meetingOptions;
     }
 
     private List<AvailabilityChange> getChangesInAvailabilityFromEvents(Collection<Event> events) {
@@ -143,5 +139,20 @@ public final class FindMeetingQuery {
         } else {
             currentAttendees.removeAll(changeAttendees);
         }
+    }
+
+    /**
+     * Given a start time and end time when attendees are available, determines if the availability is long enough and if so,
+     * adds it to a running list of meetingOptions in-place
+     *
+     * @param meetingOptions
+     * @param start
+     * @param end
+     * @param request
+     */
+    private void addMeetingOption(List<TimeRange> meetingOptions, int start, int end, long durationThreshold) {
+        TimeRange openSlot = TimeRange.fromStartEnd(start, end, false);
+        if (openSlot.duration() >= durationThreshold)
+            meetingOptions.add(openSlot);
     }
 }
